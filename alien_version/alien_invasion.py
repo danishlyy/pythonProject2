f2e64e7836cfca84e361b22e89697b9da306d610
za-liyongyong <yongyong.li@zatech.com>
2021-05-23 11:11:12 +0800

    python alien practice

--- alien_version/alien_invasion.py
+++ alien_version/alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from alien_version.settings import Settings
+from alien_version.ship import Ship
+import alien_version.game_functions as gf
 
 
 def run_game():
@@ -9,18 +11,24 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
     # 设置背景色
     # bg_color = (230,230,230)
     # 开始游戏主循环
     while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            # 每次循环时都重绘屏幕
-            screen.fill(ai_settings.bg_color)
-            if event.type == pygame.QUIT:
-                sys.exit()
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+        # for event in pygame.event.get():
+        #     # 每次循环时都重绘屏幕
+        #     screen.fill(ai_settings.bg_color)
+        #     ship.blitme()
+        #     if event.type == pygame.QUIT:
+        #         sys.exit()
+        #     # 让最近绘制的屏幕可见
+        #     pygame.display.flip()
 
 
 run_game()
--- alien_version/settings.py
+++ alien_version/settings.py
@@ -6,4 +6,6 @@ class Settings():
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        # self.bg_color = (230, 230, 240)
+        self.bg_color = (0, 0, 255)
+        self.ship_speed_factor = 1.5
