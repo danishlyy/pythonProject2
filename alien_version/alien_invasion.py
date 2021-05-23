1a280b4ab510969979ce571bebb1ccbaa5e4bec7
za-liyongyong <yongyong.li@zatech.com>
2021-05-23 14:20:36 +0800

    python alien practice 外星人增加

--- alien_version/alien_invasion.py
+++ alien_version/alien_invasion.py
@@ -4,6 +4,8 @@ from alien_version.settings import Settings
 from alien_version.ship import Ship
 import alien_version.game_functions as gf
 from pygame.sprite import Group
+from alien_version.alien import Alien
+from alien_version.game_stats import GameStats
 
 
 def run_game():
@@ -12,23 +14,32 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+    # 创建一个外星人
+    # alien = Alien(ai_settings,screen)
+    aliens = Group()
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     # 设置背景色
     # bg_color = (230,230,230)
     # 开始游戏主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        # bullets.update(bullets)
-        # 删除已消失的子弹
-        # for bullet in bullets.copy():
-        #     if bullet.rect.bottom <= 0:
-        #         bullets.remove(bullet)
-        # print(len(bullets))
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            # bullets.update(bullets)
+            # 删除已消失的子弹
+            # for bullet in bullets.copy():
+            #     if bullet.rect.bottom <= 0:
+            #         bullets.remove(bullet)
+            # print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         # 监视键盘和鼠标事件
         # for event in pygame.event.get():
         #     # 每次循环时都重绘屏幕
--- alien_version/settings.py
+++ alien_version/settings.py
@@ -10,8 +10,16 @@ class Settings():
         self.bg_color = (0, 0, 255)
         self.ship_speed_factor = 1.5
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction 为1表示向右移动，为-1表示向左移动
+        self.fleet_direction = 1
+        # 飞船设置
+        self.ship_speed_factor = 1.5
+        self.ship_limit = 3
